d1070410c627af29ca1e6d9249cf6b327be0cf70
jacop <jacopotrapani89@gmail.com>
2020-03-20 10:25:36 +0100

    Prima versione funzionante con React integrato

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,12 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import * as ReactDOM from "react-dom";
+import React from "react";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+ReactDOM.render(<div>ciao!</div>, document.getElementById('root'));
